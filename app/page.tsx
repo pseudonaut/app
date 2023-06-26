83902ceb509d2f9b0e3ba8dcea668e709ffaabff
pseudonaut <jquarnstrom@yahoo.com>
2023-06-25 17:57:35 -0700

    DEPLOY_URL removal

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Card from "@/components/home/card";
 import Balancer from "react-wrap-balancer";
-import { DEPLOY_URL } from "@/lib/constants";
 import { Github, Twitter } from "@/components/shared/icons";
 import WebVitals from "@/components/home/web-vitals";
 import ComponentGrid from "@/components/home/component-grid";
