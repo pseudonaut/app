50338f9892781847a3ffe4e2b090bf2e38946835
pseudonaut <jquarnstrom@yahoo.com>
2023-08-14 13:44:27 -0700

    GCP Implemented

--- app/api/membership/route.ts
+++ app/api/membership/route.ts
@@ -1,5 +1,7 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 import Stripe from 'stripe';
 import dbConnect from 'lib/dbConnect'
@@ -11,6 +13,19 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
 
 export async function GET(request: NextRequest) {
     
+    const session = await getServerSession(authOptions);
+
+    if (!session) {
+        return NextResponse.json(
+            {
+                error: 'Unauthorized'
+            },
+            {
+                status: 401,
+            },
+        );
+    }
+
     try {
         
         // Connect to DB
