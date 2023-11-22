ed709cc5026e60f45b50b6bc4d69065b463aa5fb
pseudonaut <jquarnstrom@yahoo.com>
2023-11-22 11:35:52 -0500

    Login Random

--- components/shared/generic.tsx
+++ components/shared/generic.tsx
@@ -3,11 +3,18 @@
 import Balancer from "react-wrap-balancer";
 import Image from "next/image";
 
+import {useTranslations} from 'next-intl';
+import { useSignInModal } from "../layout/sign-in-modal";
+
 export default function Generic() {
   
+  const t = useTranslations('Home');
+  const { SignInModal, setShowSignInModal } = useSignInModal();
+
   return (
     <>
       <div className="z-10 w-full max-w-xl px-5 xl:px-0">
+        <SignInModal />
         <br />
         <div
           style={{
@@ -27,14 +34,20 @@ export default function Generic() {
           className="animate-fade-up bg-gradient-to-br from-black to-green-600 bg-clip-text text-center font-display text-4xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-7xl md:leading-[5rem]"
           style={{ animationFillMode: "forwards" }}
         >
-          <Balancer>Solidity Nirvana</Balancer>
+          <Balancer>{t('Solidity Nirvana')}</Balancer>
         </h1>
         <h3
           className="animate-fade-up bg-gradient-to-br from-green-800 to-black bg-clip-text text-center font-display text-2xl font-bold tracking-[-0.02em] text-transparent opacity-0 drop-shadow-sm md:text-3xl md:leading-[5rem]"
           style={{ animationFillMode: "forwards" }}
         >
-          <Balancer>The ultimate guide to Solidity.</Balancer>
+          <Balancer>{t('Ultimate')}</Balancer>
         </h3>
+        <button
+            className="mt-4 inline-block w-full bg-black py-4 text-sm font-bold uppercase tracking-widest text-white hover:bg-green-500 active:bg-green-500 transition-colors duration-500"
+            onClick={() => setShowSignInModal(true)}
+          >
+            {t('Sign In')}
+          </button>
       </div>
     </>
   );
