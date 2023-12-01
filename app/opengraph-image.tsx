c6c9a6a85c2ce34f90b6643ed2491e787b6dad5b
pseudonaut <jquarnstrom@yahoo.com>
2023-12-01 18:09:02 -0500

    Reduce

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -49,8 +49,8 @@ export default async function OG() {
       </div>
     ),
     {
-      width: 1200,
-      height: 630,
+      width: 600,
+      height: 315,
       fonts: [
         {
           name: "SF Pro",
